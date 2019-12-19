8a1ad5ace2d447f607bb569a7077180781981fdb
reem <a.reem@elag.sa>
2019-12-20 01:35:53 +0300

    Updates

---
+++ precache-manifest.0eefc468a0d0a20193a5b7890ea733ca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d172c7034b779924f2a906d671004f9",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "2bf4271ca9c967ffef53",
+    "url": "/react-app/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "7588b58e32eaad6c363e",
+    "url": "/react-app/static/js/2.3a29a47f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-app/static/js/2.3a29a47f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2bf4271ca9c967ffef53",
+    "url": "/react-app/static/js/main.35c98ca1.chunk.js"
+  },
+  {
+    "revision": "f981d4500eec1e2a5e52",
+    "url": "/react-app/static/js/runtime-main.4727a4a3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
